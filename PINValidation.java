872c34ebe5642d88eb050bf88faa9f2da1c876e1
rksgr <r_kjha@yahoo.com>
2021-12-30 10:10:53 +0530

    Refactor: Allow whitespace after 3 digits in pin code

--- PINValidation.java
+++ PINValidation.java
@@ -36,9 +36,23 @@ public class PINValidation {
     Use Case 3: Regex pattern to prevent special and alphanumeric characters at end
      of pin code
     */
-    public void prvntSplCharAtEndPINValidation(){
+    public void prvntSplCharAtEndPINValidation() {
         Pattern pattern = Pattern.compile("[1-9][0-9]{5}[^a-zA-Z~!@#$%^&*()]");
         Matcher matcher = pattern.matcher("400088 ");
+        boolean pinCodeValidated = matcher.matches();
+
+        if (pinCodeValidated) {
+            System.out.println("The given pin code is correct.");
+        } else {
+            System.out.println("The pin code entered is incorrect.");
+        }
+    }
+    /*
+    Use Case 4: Regex pattern to allow space after 3 characters in the pin code
+    */
+    public void allowMidSpacePINValidation(){
+        Pattern pattern = Pattern.compile("[1-9][0-9]{5}|[1-9][0-9]{2}[\\s][0-9]{3}");
+        Matcher matcher = pattern.matcher("400 088");
         boolean pinCodeValidated = matcher.matches();
 
         if (pinCodeValidated){
@@ -51,6 +65,6 @@ public class PINValidation {
         System.out.println("Welcome to Bridgelabz!");
         System.out.println("Today we shall perform PIN code validation using Regular Expressions.");
         PINValidation pinv = new PINValidation();
-        pinv.prvntSplCharAtEndPINValidation();
+        pinv.allowMidSpacePINValidation();
     }
 }
