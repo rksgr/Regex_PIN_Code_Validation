3ac7905561b9606340106f590fabb6a802e16e16
rksgr <r_kjha@yahoo.com>
2021-12-30 09:12:25 +0530

    Added: Six digit Pin code validation

--- PINValidation.java
+++ PINValidation.java
@@ -1,6 +1,26 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
 public class PINValidation {
+    /*
+    Use Case 1: Regex pattern to validate pin code (six digit number)
+     */
+    public void sixDigitPINValidation(int pincode){
+        Pattern pattern = Pattern.compile("[1-9][0-9]{5}");
+        Matcher matcher = pattern.matcher(Integer.toString(pincode));
+        boolean pinCodeValidated = matcher.matches();
+
+        if (pinCodeValidated){
+            System.out.println("The given pin code is correct.");
+        } else{
+            System.out.println("The pin code entered is incorrect.");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Bridgelabz!");
         System.out.println("Today we shall perform PIN code validation using Regular Expressions.");
+        PINValidation pinv = new PINValidation();
+        pinv.sixDigitPINValidation(400088);
     }
 }
